c1f29c3f42c04f52dbf2ebd95dafbd496a3422b3
kmiller <kjaymiller@gmail.com>
2020-03-16 13:40:06 -0700

    all products link

--- run.py
+++ run.py
@@ -4,43 +4,47 @@ from render_engine import Site, Page, Collection
 site = Site(strict=True)
 site.SITE_TITLE = "WAXIE ULTRA"
 site.SITE_LINKS = [
-        Link('Home', '/'),
-        Link('HouseKeeping', '/housekeeping'),
-        Link('Laundry', '/Laundry'),
-        Link('Warewash', '/warewash'),
-        ]
+    Link("Home", "/"),
+    Link("HouseKeeping", "/housekeeping"),
+    Link("Laundry", "/Laundry"),
+    Link("Warewash", "/warewash"),
+]
 
 
 @site.register_collection
 class products(Collection):
     pass
 
-@site.register_route
-class Index(Page):
-    template = 'index.html'
-
 
 @site.register_route
 class AllProducts(Page):
-    template = 'all_products.html'
-    title = 'WAXIE ULTRA Products'
+    template = "all_products.html"
+    title = "WAXIE ULTRA Products"
+    slug = "all_products"
 
     def __init__(self):
         super().__init__()
-        self.products = site.collections['products'].pages
+        self.products = site.collections["products"].pages
+
 
 @site.register_route
 class Warewashing(Page):
-    template = 'all_products.html'
-    slug = 'all_warewash'
-    title = 'All Warewash Products'
+    template = "all_products.html"
+    slug = "all_warewash"
+    title = "All Warewash Products"
 
     def __init__(self):
         super().__init__()
         self.products = list(
-                filter(
-                    lambda x:getattr(x, 'category', '').lower() == 'warewash',
-                    site.collections['products'].pages)
-                )
+            filter(
+                lambda x: getattr(x, "category", "").lower() == "warewash",
+                site.collections["products"].pages,
+            )
+        )
+
+
+@site.register_route
+class Index(Page):
+    template = "index.html"
 
 site.render()
