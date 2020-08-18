ca66369591ed658a5e17c4e2c3c14cfb9bbbef97
kjaymiller <kjaymiller@gmail.com>
2020-08-18 12:12:05 -0700

    page subcollection

--- run.py
+++ run.py
@@ -17,7 +17,7 @@ site.strict=True
 site.SITE_URL = "https://wultra-hamilton-5d107b.netlify.com"
 site.SITE_TITLE = "WAXIE ULTRA"
 site.search = Fuse
-site.output_path = 'docs'
+site.output_path = Path('docs')
 
 site.categories = [
     hfm_link,
