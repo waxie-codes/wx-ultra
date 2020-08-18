7de0820fdc4898fd9cb958482aaeae350c1320d1
kjaymiller <kjaymiller@gmail.com>
2020-08-18 11:40:44 -0700

    make sites local

--- run.py
+++ run.py
@@ -12,7 +12,8 @@ hfm_link = Link(
 laundry_link = Link("Laundry", "/laundry")
 warewash_link = Link("Warewash", "/warewash")
 
-site = Site(strict=True)
+site = Site()
+site.strict=True
 site.SITE_URL = "https://wultra-hamilton-5d107b.netlify.com"
 site.SITE_TITLE = "WAXIE ULTRA"
 site.search = Fuse
@@ -53,33 +54,15 @@ class HouseKeeping(Page):
     content_path = "content/pages/housekeeping_facility_maint.md"
     template = "all_products.html"
 
-    def __init__(self):
-        pages = [x for x in Products().pages if x.category == "housekeeping & facility maintenance"]
-        self.pages = list(sorted(pages, key=lambda x: x.waxie_item_number))
-        super().__init__()
-
-
 class Laundry(Page):
     slug = "laundry"
     content_path = "content/pages/laundry.md"
     template = "all_products.html"
 
-    def __init__(self):
-        pages = [x for x in Products().pages if x.category == "laundry"]
-        self.pages = list(sorted(pages, key=lambda x: x.waxie_item_number))
-        super().__init__()
-
-
 class Warewash(Page):
     slug = "warewash"
     content_path = "content/pages/warewash.md"
     template = "all_products.html"
-
-    def __init__(self):
-        pages = [x for x in Products().pages if x.category == "warewash"]
-        self.pages = list(sorted(pages, key=lambda x: x.waxie_item_number))
-        super().__init__()
-
 
 site.route(HouseKeeping())
 site.route(Warewash())
